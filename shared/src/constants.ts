5729bdad4de99ae23d8cdaa3bab23b6e8759b0ed
Štěpán Krouský <stepan.krousky@seznam.cz>
2026-01-05 00:20:40 +0100

    update ports

--- shared/src/constants.ts
+++ shared/src/constants.ts
@@ -1,5 +1,5 @@
 export const CHANNEL_NAME = 'd4rzk'
 
-export const SERVER_HTTP_PORT = 3000
-export const OVERLAY_TERMINAL_PORT = 5175
-export const OVERLAY_CHAT_PORT = 5176
+export const SERVER_HTTP_PORT = 4000
+export const OVERLAY_TERMINAL_PORT = 4001
+export const OVERLAY_CHAT_PORT = 4002
